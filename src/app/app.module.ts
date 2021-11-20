4925cf4f62e94c99bfff0daedbe61725b7553b81
J. Derksen <jip.derksen@hva.nl>
2021-11-20 14:07:08 +0100

    style, routing and some other stuff work now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { AboutComponent } from './components/mainpage/about/about.component';
 import { ContactComponent } from './components/mainpage/contact/contact.component';
 import { ProjectsComponent } from './components/mainpage/projects/projects.component';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './components/error/page-not-found/page-not-found.component';
+import { HeaderComponent } from './components/header/header/header.component';
 
 const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent }
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ]
 
 @NgModule({
@@ -18,7 +25,9 @@ const appRoutes: Routes = [
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    PageNotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
