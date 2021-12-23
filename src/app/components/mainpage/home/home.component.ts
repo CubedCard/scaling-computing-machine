2e8fdff1bca103b7968c83620972fccd5b2509d4
J. Derksen <jip.derksen@hva.nl>
2021-12-23 14:08:07 +0100

    add random programming / christmas jokes tot the home page

--- src/app/components/mainpage/home/home.component.ts
+++ src/app/components/mainpage/home/home.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Joke } from '../../../models/joke';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    setup: string;
+    punch: string;
 
-  constructor() { }
+    constructor(private http: HttpClient) { 
+        this.setup = "";
+        this.punch = "";
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getJoke().subscribe((joke: Joke) => {
+            console.log(joke);
+            this.setup = joke.setup;
+            this.punch = joke.delivery;
+        });
+        this.startTimedJoke();
+    }
+
+    getJoke(): Observable<Joke> {
+        return this.http.get<Joke>("https://v2.jokeapi.dev/joke/Programming,Christmas?type=twopart");
+    }
+
+    startTimedJoke() {
+        setInterval(() => {
+            this.getJoke().subscribe((joke: Joke) => {
+                console.log(joke);
+                this.setup = joke.setup;
+                this.punch = joke.delivery;
+            });
+        }, 7000); 
+    }
 
 }
