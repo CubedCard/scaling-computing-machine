b13b33eae63f4413dd548acb916d8d32e8cc8e64
Jip <78497126+CubedCard@users.noreply.github.com>
2025-02-28 10:25:56 +0100

    Undo changes

--- src/app/components/mainpage/about/about.component.ts
+++ src/app/components/mainpage/about/about.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
     age: number;
     skills: string[];
 
@@ -28,6 +28,9 @@ export class AboutComponent {
             "SQL and NoSQL Databases",
             "HTML, CSS and JavaScript"
         ];
+    }
+    
+    ngOnInit(): void {
     }
 
 }
