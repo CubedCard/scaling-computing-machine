20079610c925df7007d99447d4383b283a1ae995
Jip Derksen <78497126+CubedCard@users.noreply.github.com>
2026-02-25 22:06:05 +0100

    WIP: Added redirect route for mismatched paths
    - Added a route that redirects the user if there was no path found.
    - If, however, the user uses curl to get the non-existing page, the
      redirect HTML is returned, which is not ideal.
    - This needs to be changed.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
 from routes.pages import register_routes
 
 app = Flask(__name__)
 
 # register all routes
 register_routes(app)
+
+@app.route('/<name>')
+def match_all(name=None):
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
