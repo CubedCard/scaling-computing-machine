482eec30b767168056aab96342d05cb8a9d4e9ec
Jip <78497126+CubedCard@users.noreply.github.com>
2026-04-14 14:07:33 +0200

    453. redirect all unknown paths to landing (#455)
    * WIP: Added redirect route for mismatched paths
    
    - Added a route that redirects the user if there was no path found.
    - If, however, the user uses curl to get the non-existing page, the
      redirect HTML is returned, which is not ideal.
    - This needs to be changed.
    
    * Added redirect to home for all unknown paths
    
    * Removed cache
    
    * Removed unused imports

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-from routes.pages import register_routes
+from flask import Flask, request, redirect
+from routes.pages import register_routes, term_pages, wants_html
 
 app = Flask(__name__)
 
 # register all routes
 register_routes(app)
+
+@app.route('/<path:name>')
+def match_all(name=None):
+    if wants_html(request):
+        return redirect("/", code=302)
+    return term_pages["index"]()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
